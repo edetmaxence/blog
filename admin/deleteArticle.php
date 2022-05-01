2208eef093a2f4c71fd43fbbd30821f927e573b6
edetmaxence <edet.maxence@gmail.com>
2022-05-01 17:31:56 +0200

    delete

--- admin/deleteArticle.php
+++ admin/deleteArticle.php
@@ -6,31 +6,27 @@ if(isset($_SESSION["admin"])):
     require_once '../vendor/autoload.php';
 
    
+    $id = htmlspecialchars(strip_tags($_GET['id']));
 
-    $idSupp= $_GET["idpost"];
-    
+    // Supprime l'article via son ID
+    $query = $db->prepare('DELETE FROM posts WHERE idpost = :id');
+    $query->bindValue(':id', $id, PDO::PARAM_INT);
+    $query->execute();
+ 
+    // Si aucun ligne n'a été affectées par la suppression, on redirige vers une erreur 404
+if ($query->rowCount() === 0) {
+	header('Location: ../404.php');
+}
+else {
+	// Redirection vers la page d'accueil de l'admin en cas de succès
+	header('Location: index.php?successDelete=1');
+}
 
 ?>
 
 
 
-<div class="modal" tabindex="-1">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <p><?php dump($idSupp); ?></p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+
 
 
 <?php
