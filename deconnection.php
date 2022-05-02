b7005497abb28ad35a771c1d139afcae58ca1c75
edetmaxence <edet.maxence@gmail.com>
2022-05-02 12:03:41 +0200

    deconnexion

--- deconnection.php
+++ deconnection.php
@@ -2,5 +2,17 @@
 session_start();
 unset($_SESSION["user"]);
 unset($_SESSION["admin"]);
-
+session_destroy();
 header("Location: /");
+
+
+/**
+ * Detruit toute les variables  sessions existante
+ */
+/*
+ session_unset();
+ 
+ //detruit toutes les sessions existantes
+ session_destroy();
+
+ */
